843f8f17066dcec3c7f2fdd6a5911524597e58e8
Last-dreamer <asim0000.ak@gmail.com>
2023-09-07 22:17:44 +0400

    change the main api

--- lib/data/data_source/grpahql_config.dart
+++ lib/data/data_source/grpahql_config.dart
@@ -2,7 +2,7 @@ import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GraphQlConfig {
   //! before api link  http://20.203.31.58/queryquery
-  static HttpLink link = HttpLink("http://127.0.0.1/query");
+  static HttpLink link = HttpLink("http://62.72.59.126:8001/query");
   GraphQLClient clientToQuery() =>
       GraphQLClient(link: link, cache: GraphQLCache());
 }
