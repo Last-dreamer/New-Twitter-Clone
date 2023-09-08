99364fb4fa88798ad506132bf33e17c9ed286007
Last-dreamer <asim0000.ak@gmail.com>
2023-09-08 22:18:54 +0400

    adding one comment

--- lib/data/data_source/grpahql_config.dart
+++ lib/data/data_source/grpahql_config.dart
@@ -1,7 +1,9 @@
 import 'package:graphql_flutter/graphql_flutter.dart';
 
 class GraphQlConfig {
+  // ! older dashboard  api
   //! before api link  http://20.203.31.58/queryquery
+
   static HttpLink link = HttpLink("http://62.72.59.126:8001/query");
   GraphQLClient clientToQuery() =>
       GraphQLClient(link: link, cache: GraphQLCache());
